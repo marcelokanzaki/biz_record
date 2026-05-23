0b1ff3e5ca32298a3f7900ca4f4cb6145fa652f4
Marcelo Kanzaki <marcelo@hey.com>
2026-05-23 13:41:05 -0300

    Organize schedule modules

--- lib/biz_record.rb
+++ lib/biz_record.rb
@@ -3,8 +3,8 @@
 require "active_record"
 require "biz"
 require_relative "biz_record/version"
-require_relative "biz_record/holidays"
-require_relative "biz_record/weekly_hours"
+require_relative "biz_record/schedule/holidays"
+require_relative "biz_record/schedule/weekly_hours"
 require_relative "biz_record/schedule"
 require_relative "biz_record/railtie" if defined?(Rails::Railtie)
 
