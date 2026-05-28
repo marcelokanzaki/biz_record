162dccc8bf0a40355f939b424788743d700e0ee3
Marcelo Kanzaki <marcelo@hey.com>
2026-05-27 22:14:28 -0300

    Changes delete strategy of schedule days

--- app/models/biz_record/schedule.rb
+++ app/models/biz_record/schedule.rb
@@ -9,7 +9,7 @@ module BizRecord
     belongs_to :schedulable, polymorphic: true, optional: false
 
     has_many :intervals,    -> { chronological }, class_name: "BizRecord::Interval", as: :owner, dependent: :delete_all
-    has_many :days,         -> { chronological }, class_name: "BizRecord::Day", dependent: :destroy, inverse_of: :schedule
+    has_many :days,         -> { chronological }, class_name: "BizRecord::Day", dependent: :delete_all, inverse_of: :schedule
     has_many :shift_days,   -> { chronological }, class_name: "BizRecord::Days::Shift", inverse_of: :schedule
     has_many :break_days,   -> { chronological }, class_name: "BizRecord::Days::Break", inverse_of: :schedule
     has_many :holiday_days, -> { chronological }, class_name: "BizRecord::Days::Holiday", inverse_of: :schedule
